163aaa1efaef17857a173ab9f378e04107ff6f07
Ming Yi <myi@student.ethz.ch>
2020-10-10 21:51:50 +0200

    Updates

---
+++ precache-manifest.aa21b22b66ea30f7c7988a4b08dd23f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "709676442dabb51b3e69079fd44b453e",
+    "url": "/zurich-drifter/index.html"
+  },
+  {
+    "revision": "8e125865212368d8e8b8",
+    "url": "/zurich-drifter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d4d9c74d6d3d451eab7",
+    "url": "/zurich-drifter/static/js/2.09344ecb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zurich-drifter/static/js/2.09344ecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e125865212368d8e8b8",
+    "url": "/zurich-drifter/static/js/main.88dd4ff8.chunk.js"
+  },
+  {
+    "revision": "04af840ca5a8edd3b77a",
+    "url": "/zurich-drifter/static/js/runtime-main.32b6ad3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zurich-drifter/static/media/logo.5d5d9eef.svg"
+  }
+]);
